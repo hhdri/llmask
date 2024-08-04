80018d941cef85dca9b8915c99c98e1fe540220f
Majid Hajiheidari <majidhhdri@gmail.com>
2024-08-04 03:39:44 +0200

    Add postgres-data to gitignore

--- model_manager/models.py
+++ model_manager/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class LLM(models.Model):
-    provider_name = models.CharField(max_length=100)
-    model_name_version = models.CharField(max_length=100)
+    provider_name = models.CharField(max_length=100, null=False)
+    model_name_version = models.CharField(max_length=100, null=False)
 
     def __str__(self):
         return f"{self.provider_name} - {self.model_name_version}"
