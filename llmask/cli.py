99bc8ff1b54bce79b497c64a107947b75d8713f5
Majid Hajiheidari <majidhhdri@gmail.com>
2024-08-24 23:37:04 +0200

    Working cli

--- llmask/cli.py
+++ llmask/cli.py
@@ -1,7 +1,140 @@
+import os
 import argparse
+from functools import partial
+
+from groq import Groq
+from openai import OpenAI
+
+
+MAX_TOKENS = 1024
+TEMPERATURE = 1
+
+
+class NvidiaLLM:
+    @staticmethod
+    def get_response(
+        model_name: str, system_prompt: str, user_prompt: str, random_seed: int
+    ):
+        client = OpenAI(
+            base_url="https://integrate.api.nvidia.com/v1",
+            api_key=os.environ["NVIDIA_API_KEY"],
+        )
+        completion = client.chat.completions.create(
+            model=model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            temperature=TEMPERATURE,
+            max_tokens=MAX_TOKENS,
+            top_p=1,
+            stream=False,
+            stop=None,
+            seed=random_seed,
+        )
+
+        return completion.choices[0].message.content
+
+
+class GroqLLM:
+    @staticmethod
+    def get_response(
+        model_name: str, system_prompt: str, user_prompt: str, random_seed: int
+    ):
+        client = Groq()
+        completion = client.chat.completions.create(
+            model=model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            temperature=TEMPERATURE,
+            max_tokens=MAX_TOKENS,
+            top_p=1,
+            stream=False,
+            stop=None,
+            seed=random_seed,
+        )
+
+        return completion.choices[0].message.content
+
+
+class OpenAILLM:
+    @staticmethod
+    def get_response(
+        model_name: str, system_prompt: str, user_prompt: str, random_seed: int
+    ):
+        client = OpenAI()
+        completion = client.chat.completions.create(
+            model=model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            temperature=TEMPERATURE,
+            max_tokens=MAX_TOKENS,
+            top_p=1,
+            seed=random_seed,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+
+        return completion.choices[0].message.content
+
+
+model_slugs = {
+    "gpt-4o-mini-v1": partial(
+        OpenAILLM.get_response, model_name="gpt-4o-mini-2024-07-18"
+    ),
+    "gpt-4o-v1": partial(OpenAILLM.get_response, model_name="gpt-4o-2024-08-06"),
+    "groq-llama-3.1-70b": partial(
+        GroqLLM.get_response, model_name="llama-3.1-70b-versatile"
+    ),
+    "groq-llama-3.1-8b": partial(
+        GroqLLM.get_response, model_name="llama-3.1-8b-instant"
+    ),
+    "nvidia-llama-3.1-8b": partial(
+        NvidiaLLM.get_response, model_name="meta/llama-3.1-8b-instruct"
+    ),
+    "nvidia-llama-3.1-70b": partial(
+        NvidiaLLM.get_response, model_name="meta/llama-3.1-70b-instruct"
+    ),
+    "nvidia-llama-3.1-405b": partial(
+        NvidiaLLM.get_response, model_name="meta/llama-3.1-405b-instruct"
+    ),
+}
+
+system_prompts = {
+    "bash-v1": "You're an assistant who provides bash script commands for Linux and MacOS. Output the command as per user's request with no additional explanations. Don't format in markdown.",
+    "define-v1": "You're an assistant who helps to define and explain words/terms/persons to the user like a dictionary or wikipedia. It's important to keep it short and brief.",
+    "vim-v1": "You're an assistant who helps with vim/neovim commands. User may ask about a shortcut or command in vim/neovim to do a particular thing.",
+    "stat-v1": "You're an assistant who help user with statistics and machine learning questions. User is a data scientist with a B.Sc. degree in Statistics familiar with deep learning and data engineering stuff.",
+}
+
 
 def cli_entrypoint():
-    parser = argparse.ArgumentParser(description="My CLI tool.")
-    parser.add_argument("name", help="Your name.")
+    parser = argparse.ArgumentParser(
+        description="Ask a question from a llm and get a response."
+    )
+
+    parser.add_argument(
+        "--llm",
+        default="gpt-4o-mini-v1",
+        choices=model_slugs.keys(),
+    )
+    parser.add_argument("--random_seed", type=int, default=42)
+    parser.add_argument(
+        "system_prompt",
+        choices=system_prompts.keys()
+    )
+    parser.add_argument("user_prompt")
+
     args = parser.parse_args()
-    print(f"Hello, {args.name}!")
+
+    response = model_slugs[args.llm](
+        system_prompt=system_prompts[args.system_prompt],
+        user_prompt=args.user_prompt,
+        random_seed=args.random_seed,
+    )
+
+    print(response)
